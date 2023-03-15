7c5e5aaad12abe1dd49bd5933c794ce8eefe68bd
ahmedopank <ahmedopank@gmail.com>
2023-03-16 00:36:58 +0700

    add animation to background and header title

--- src/js/components/header/header.js
+++ src/js/components/header/header.js
@@ -1,53 +1,65 @@
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import webLogo from '../../../res/icon.svg';
-import Headroom from 'react-headroom';
-import './hamburger.css';
-import './header.css';
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import webLogo from "../../../res/icon.svg";
+import Headroom from "react-headroom";
+import React, { useState, useEffect } from "react";
+import "./hamburger.css";
+import "./header.css";
 
 function Header() {
     const onToggleFunction = function (expanded) {
-        const bg = document.getElementById('wx-background');
-        const burger = document.getElementById('hamburger');
+        const burger = document.getElementById("hamburger");
+
         if (expanded) {
-            bg.classList.add('collapsed');
-            burger.classList.add('is-active');
+            burger.classList.add("is-active");
         } else {
-            bg.classList.remove('collapsed');
-            burger.classList.remove('is-active');
+            burger.classList.remove("is-active");
         }
-    }
+    };
 
     const onPinFunc = () => {
-        document.getElementById('nav-container').classList.add('bg-add');
+        document.getElementById("nav-container").classList.add("bg-add");
         if (window.innerWidth < 992) {
-            document.getElementById('nav-collapse').classList.remove('bg-add');
+            document.getElementById("nav-collapse").classList.remove("bg-add");
         }
-    }
+    };
 
     const onUnfixFunc = () => {
-        document.getElementById('nav-container').classList.remove('bg-add');
+        document.getElementById("nav-container").classList.remove("bg-add");
         if (window.innerWidth < 992) {
-            document.getElementById('nav-collapse').classList.add('bg-add');
+            document.getElementById("nav-collapse").classList.add("bg-add");
         }
-    }
+    };
+
+    const [titleShow, setTitleShow] = useState(false);
+
+    useEffect(() => {
+        function handleScroll() {
+            const position = window.scrollY;
+            setTitleShow(position > 150);
+        }
+
+        window.addEventListener("scroll", handleScroll);
+        return () => {
+            window.removeEventListener("scroll", handleScroll);
+        };
+    }, []);
 
     return (
         <Headroom onPin={onPinFunc} onUnfix={onUnfixFunc}>
-            <Navbar collapseOnSelect expand="lg" className='navbar-custom' onToggle={onToggleFunction}>
-                <Container className='container-md' id="nav-container">
-                    <Navbar.Brand id="navbar-brand" href="#home" className='d-flex align-items-center'>
-                        <img
-                            src={webLogo}
-                            width="32"
-                            height="32"
-                            alt="Liska Feby logo"
-                        />
+            <Navbar collapseOnSelect expand="lg" className="navbar-custom" onToggle={onToggleFunction}>
+                <Container className="container-md" id="nav-container">
+                    <Navbar.Brand id="navbar-brand" href="#home" className="d-flex align-items-center">
+                        <img src={webLogo} width="32" height="32" alt="Liska Feby logo" />
                     </Navbar.Brand>
 
+                    <div id="navbar-title" className="navbar-title" data-is-hidden={titleShow}>
+                        Liska Feby
+                    </div>
+
                     <Navbar.Toggle aria-controls="responsive-navbar-nav" as="div">
-                        <button id='hamburger' className="hamburger hamburger--slider" type="button">
+                        <button id="hamburger" className="hamburger hamburger--slider" type="button">
                             <span className="hamburger-box">
                                 <span className="hamburger-inner"></span>
                             </span>
@@ -55,10 +67,19 @@ function Header() {
                     </Navbar.Toggle>
                     <Navbar.Collapse id="responsive-navbar-nav">
                         <Nav id="nav-collapse" className="me-auto bg-add">
-                            <Nav.Link href="#home" className='m-0'>Home</Nav.Link>
-                            <Nav.Link href="#about" className='m-0'>About</Nav.Link>
-                            <Nav.Link href="#portfolio" className='m-0'>Portfolio</Nav.Link>
-                            <Nav.Link href="#" className='d-flex align-items-center gap-1 m-0'>Blog<span className="badge rounded-pill badge-custom px-2 mx-1">coming soon</span></Nav.Link>
+                            <Nav.Link href="#home" className="m-0">
+                                Home
+                            </Nav.Link>
+                            <Nav.Link href="#about" className="m-0">
+                                About
+                            </Nav.Link>
+                            <Nav.Link href="#portfolio" className="m-0">
+                                Portfolio
+                            </Nav.Link>
+                            <Nav.Link href="#" className="d-flex align-items-center gap-1 m-0">
+                                Blog
+                                <span className="badge rounded-pill badge-custom px-2 mx-1">coming soon</span>
+                            </Nav.Link>
                             <Nav.Link href="#contact">Contact</Nav.Link>
                         </Nav>
                     </Navbar.Collapse>
--- src/js/containers/hero/hero.js
+++ src/js/containers/hero/hero.js
@@ -1,23 +1,51 @@
-import heroPng from '../../../res/header.png';
-import bg from '../../../res/background.svg'
-import { Fade } from 'react-awesome-reveal';
-import './hero.css';
+import heroPng from "../../../res/header.png";
+import bg from "../../../res/background.svg";
+import { Fade } from "react-awesome-reveal";
+import "./hero.css";
+import { useState, useEffect, useRef } from "react";
 
 function Hero() {
+    const [posY, setPosY] = useState(0);
+    const [scale, setScale] = useState(1);
+    const myRef = useRef(null);
+
+    useEffect(() => {
+        function handleScroll() {
+            const position = window.scrollY;
+            const height = (window.screen.height - 540) / 2;
+            if (height < 0) {
+                return;
+            }
+            setPosY(Math.min((height * position) / 500, height));
+            setScale(Math.min((0.4 * position) / 500 + 1, 1.4));
+        }
+
+        window.addEventListener("scroll", handleScroll);
+        return () => {
+            window.removeEventListener("scroll", handleScroll);
+        };
+    });
+
+    useEffect(() => {
+        if (myRef.current) {
+            const transform = `translateY(${posY}px) scale(${scale})`;
+            myRef.current.style.transform = transform;
+        }
+    }, [posY]);
+
     return (
         <section id="hero">
-            <Fade triggerOnce={true} >
-                <div id='wx-background' className="wx-background">
-                    <img
-                        src={bg}
-                        alt="background"
-                    />
-                </div>
-                <div className='d-flex flex-column align-items-center container-custom-bg'>
-                    <div id="home" className='web-title'>Liska Feby</div>
-                    <div className='web-subtitle'>Reveal your best skin with trusted reviews</div>
-                    <div id="about" className='meja'></div>
-                    <div className='hero-image'>
+            <div id="wx-background" className="wx-background" ref={myRef}>
+                <img src={bg} alt="background" />
+            </div>
+            <Fade triggerOnce={true}>
+                <div className="d-flex flex-column align-items-center container-custom-bg">
+                    <div id="home" className="web-title">
+                        Liska Feby
+                    </div>
+                    <div className="web-subtitle">Reveal your best skin with trusted reviews</div>
+                    <div id="about" className="meja"></div>
+                    <div className="hero-image">
                         <img src={heroPng} alt="Liska Feby" width="320" height="320" />
                     </div>
                 </div>
